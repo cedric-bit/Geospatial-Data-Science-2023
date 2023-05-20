154ef2533b90e5ad4f812f2d658a9a1bdec7c3ff
timarque <56795474+timarque@users.noreply.github.com>
2023-05-20 17:15:43 +0200

    Date change

--- App.py
+++ App.py
@@ -26,8 +26,11 @@ class App(QMainWindow):
         self.top = 124
         self.width = 1600
         self.height = 720
-        self.current_date = "30/03/2023"
-        self.path = 'C:\Work\geospatial'
+        self.path = 'C:\Work\geospatial\\2023-03-30'
+        self.base_path = 'C:\Work\geospatial\\'
+        self.add_path = ['2023-03-18', '2023-03-19', '2023-03-20', '2023-03-21', '2023-03-22', '2023-03-23',
+                        '2023-03-24', '2023-03-25', '2023-03-26', '2023-03-27', '2023-03-28', '2023-03-29',
+                         '2023-03-30']
         self.gtfs_files = []
         self.gtfsrt_files = []
         self.gtfs_file_path = ""
@@ -37,9 +40,10 @@ class App(QMainWindow):
         self.routes_df = []
         self.trip_updates = []
         self.trip_updates_with_schedule = []
+        self.file_number = 12
 
-        self.initUI()
         self.load_data()
+        self.initUI()
 
     def initUI(self):
         """
@@ -48,11 +52,11 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.search_bar_label = QLabel(self)
-        self.search_bar_label.setText("  Type stop name to \ncheck train stop times")
-        self.search_bar_label.move(115, 30)
-        self.search_bar_label.resize(150, 50)
-        self.search_bar_label.setFont(QFont("Arial", 10))
+        #self.search_bar_label = QLabel(self)
+        #self.search_bar_label.setText("  Type stop name to \ncheck train stop times")
+        #self.search_bar_label.move(115, 30)
+        #self.search_bar_label.resize(150, 50)
+        #self.search_bar_label.setFont(QFont("Arial", 10))
 
         mainMenu = self.menuBar()
         exitMenu = mainMenu.addMenu('Quitter')
@@ -72,13 +76,13 @@ class App(QMainWindow):
         self.dash_title.setFont(QFont("Arial", 16))
         self.dash_title.move(700, 20)
 
-        self.hbox = QHBoxLayout()
-        self.search_bar = QLineEdit(self)
-        self.search_bar.resize(200, 20)
-        self.search_bar.move(75, 75)
-        self.hbox.addWidget(self.search_bar)
-        self.setLayout(self.hbox)
-        self.search_bar.returnPressed.connect(lambda: self.check_stop(self.search_bar))
+        #self.hbox = QHBoxLayout()
+        #self.search_bar = QLineEdit(self)
+        #self.search_bar.resize(200, 20)
+        #self.search_bar.move(75, 75)
+        #self.hbox.addWidget(self.search_bar)
+        #self.setLayout(self.hbox)
+        #self.search_bar.returnPressed.connect(lambda: self.check_stop(self.search_bar))
 
         self.average_label = QLabel(self)
         self.average_label.setText("Most delayed trains")
@@ -94,6 +98,8 @@ class App(QMainWindow):
         self.table.move(1230, 100)
 
         self.buttons()
+        self.change_day()
+        self.show_delays()
 
         self.show()
 
@@ -111,55 +117,53 @@ class App(QMainWindow):
     def show_delays(self):
         # To do : get data about delays and show it on the left side of the map
         # maybe add total delay of each train line and give top delayed line
-        print("Delays")
+        self.big_delay = QLabel(self)
+        big_del, big_del_route = self.biggest_delay()
+        big_del //= 60
 
-    def check_stop(self, search_bar):
-        # checks if the stop name is in the db, for now it only works with "Gare du Nord" write it as it is
-        self.stop = self.search_bar.text()  # gets text from the search bar
-        if self.stop in self.stops_df['stop_id'].values[0]:
-            self.show_stop(search_bar)
-        else:
-            self.search_bar_label.setText("This stop does not exist")
+        self.big_delay.setText("Biggest delay of the day is " + str(big_del) + " minutes on route :" + str(big_del_route))
+        self.big_delay.move(75, 75)
+        self.big_delay.resize(400, 400)
 
-    def show_stop(self, search_bar):
+    def biggest_delay(self):
+        biggest = 0
+        route = None
+        for delay in self.trip_updates:
+            if delay['delay'] > biggest:
+                biggest = delay['delay']
+                route = delay['route_id']
+
+        return biggest, route
+
+    def change_day(self):
         # user types stop name, give times of each train passing in this stop, choose date for this aswell
-        self.stop = self.search_bar.text()  # gets text from the search bar
-        self.stop_window = QMainWindow(self)
-        self.stop_window.setWindowTitle("Stop window")
-        self.stop_window.setGeometry(self.left, self.top, self.width, self.height)  # probably change these values
-        self.date = QLabel(self.stop_window)
-        self.date.setText(self.current_date)
-        self.date.move(770, 50)
-        self.incr_date = QPushButton(">", self.stop_window)
-        self.decr_date = QPushButton("<", self.stop_window)
-        self.decr_date.move(650, 50)
-        self.incr_date.move(850, 50)
+        self.date = QLabel(self)
+        self.date.setText(self.add_path[self.file_number])
+        self.date.move(170, 50)
+        self.incr_date = QPushButton(">", self)
+        self.decr_date = QPushButton("<", self)
+        self.decr_date.move(50, 50)
+        self.incr_date.move(250, 50)
         self.incr_date.clicked.connect(lambda: self.new_date(1))
         self.decr_date.clicked.connect(lambda: self.new_date(-1))
-
-        self.stop_table = QTableWidget(self.stop_window)
-        self.stop_table.setColumnCount(3)
-        self.stop_table.setHorizontalHeaderLabels(["Line n°", "Scheduled arrival time", "Delay"])
-        self.stop_table.setRowCount(10)  # set row count to however many trains passing that day
-        self.stop_table.resize(400, 600)
-        self.stop_table.move(590, 100)
-        self.stop_table.horizontalHeader().setSectionResizeMode(
-            QHeaderView.Stretch)  # Stretch columns to fill the available space
-
-        self.stop_title = QLabel(self.stop_window)
-        self.stop_title.resize(300, 40)
-        self.stop_title.setText(self.search_bar.text())  # Stop name as title
-        self.stop_title.setFont(QFont("Arial", 16))
-        self.stop_title.move(740, 20)
-
-        self.stop_window.show()
 
     def new_date(self, modification):
         # move from day to day, get real data instead of "next date" etc
         if modification == 1:
-            self.date.setText("next date")
+            if self.file_number == 12:
+                self.file_number = 0
+            else:
+                self.file_number += 1
         else:
-            self.date.setText("previous date")
+            if self.file_number == 0:
+                self.file_number = 12
+            else:
+                self.file_number -= 1
+
+        self.date.setText(self.add_path[self.file_number])
+        self.path = self.base_path + self.add_path[self.file_number]
+        self.open_files()
+        self.real_time_gtfs()
 
     #    ------------------------------------------ Data handling ------------------------------------------
 
@@ -170,12 +174,11 @@ class App(QMainWindow):
     def read_gtfs_static_file(self, file_path):
         # print(f"Ouverture du fichier {file_path}")
         with zipfile.ZipFile(file_path, 'r') as z:
-            stops_df = pd.read_csv(z.open('stops.txt'))
-            stop_times_df = pd.read_csv(z.open('stop_times.txt'))
-            trips_df = pd.read_csv(z.open('trips.txt'))
-            routes_df = pd.read_csv(z.open('routes.txt'))
+            self.stops_df = pd.read_csv(z.open('stops.txt'))
+            self.stop_times_df = pd.read_csv(z.open('stop_times.txt'))
+            self.trips_df = pd.read_csv(z.open('trips.txt'))
+            self.routes_df = pd.read_csv(z.open('routes.txt'))
 
-        return stops_df, stop_times_df, trips_df, routes_df
 
     def open_files(self):
         for subdir, dirs, files in os.walk(self.path):
@@ -189,7 +192,7 @@ class App(QMainWindow):
         # Utilisez les fonctions pour extraire les informations
 
         for gtfs_file in self.gtfs_files:
-            self.stops_df, self.stop_times_df, self.trips_df, self.routes_df = self.read_gtfs_static_file(gtfs_file)
+            self.read_gtfs_static_file(gtfs_file)
             # Utilisez les DataFrames pour effectuer vos analyses et traitements
 
     def extract_trip_updates(self, feed_realtime):
@@ -203,7 +206,6 @@ class App(QMainWindow):
                     arrival_time = stop_time_update.arrival.time
                     departure_time = stop_time_update.departure.time
                     delay = stop_time_update.arrival.delay
-
                     self.trip_updates.append({
                         'trip_id': trip_id,
                         'route_id': route_id,
@@ -230,13 +232,9 @@ class App(QMainWindow):
         for update in self.trip_updates:
             trip_id = update['trip_id']
             stop_id = update['stop_id']
-
-            stop_time = self.stop_times_df.loc[
-                (self.stop_times_df['trip_id'] == trip_id) & (
-                            self.stop_times_df['stop_id'] == stop_id), 'arrival_time'].values
+            stop_time = self.stop_times_df.loc[(self.stop_times_df["trip_id"] == trip_id) & (self.stop_times_df["stop_id"] == stop_id), "arrival_time"].values
             if len(stop_time) > 0:
                 scheduled_arrival_time = stop_time[0]
-
                 self.trip_updates_with_schedule.append({
                     'trip_id': trip_id,
                     'route_id': update['route_id'],
@@ -247,10 +245,11 @@ class App(QMainWindow):
                 })
 
     def real_time_gtfs(self):
-        feed_realtime = self.process_gtfsrt_file(self.gtfsrt_files[0])
+        feed_realtime = self.process_gtfsrt_file(self.gtfsrt_files[-1])
         if feed_realtime is not None:
             self.extract_trip_updates(feed_realtime)
-            self.merge_trip_updates_and_schedule()
+            # self.merge_trip_updates_and_schedule()
+
 
 def main():
     app = QApplication(sys.argv)
